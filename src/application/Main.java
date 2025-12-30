dbc7caf4b67c08e85be18c4e619587a74b46592f
SergioCostaczr <manoelsergio.dev@gmail.com>
2025-12-30 05:05:07 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department dp = new Department(1,"books");
+        System.out.println(dp);
+    }
+}
